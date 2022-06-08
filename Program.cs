3784b1e14549008185c73c8412524718fb762d87
nazap31 <you@example.com>
2022-06-08 12:44:16 -0600

    my second commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("variables");
+// int
+int age = 21;
+Console.WriteLine(age);
+
+int roomNumber;
+roomNumber = 4;
+Console.WriteLine(roomNumber);
+
+//double
+double pi = 3.14D;
+Console.WriteLine(pi);
+
+// char
+char firstLetter = 'A';
+Console.WriteLine(firstLetter);
+
+// bool
+bool accepted = false;
+Console.WriteLine(accepted);
+
+bool complete = true;
+Console.WriteLine(complete);
+
+// string
+string lastName = "Naza";
+Console.WriteLine(lastName);
+
+// programa de lectura y escritura I/0
+string fullName;
+Console.WriteLine("Ingrese su nombre completo");
+fullName = Console.ReadLine();
+
+Console.Write("Su nombre es:");
+Console.Write(fullName);
+
+//----
+Console.Write("\nEn que pais vive usted?");
+string country = Console.ReadLine();
+
+Console.WriteLine ("Su pais es: ");
+Console.WriteLine (country);
